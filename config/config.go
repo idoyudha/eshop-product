1669fb3acf4516af1137c5f1257eefa6dd0698b0
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-07 12:48:25 +0700

    feat: init config

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type (
+	// Config
+	Config struct {
+		App  `yaml:"app"`
+		HTTP `yaml:"http"`
+	}
+
+	// App -.
+	App struct {
+		Name    string `env-required:"true" yaml:"name"    env:"APP_NAME"`
+		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+	}
+
+	// HTTP -.
+	HTTP struct {
+		Port string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
+	}
+)
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig("./config/config.yml", cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
