667f9bf32a766c429ad3af4038b97044ab15e0f3
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-01 21:32:14 +0700

    feat: add cors origin

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/idoyudha/eshop-product/internal/usecase"
 	"github.com/idoyudha/eshop-product/pkg/logger"
@@ -14,9 +15,14 @@ func HTTPNewRouter(
 	ucg usecase.Category,
 	l logger.Interface,
 ) {
-	// options
-	// handler.Use(gin.Logger())
-	// handler.Use(gin.Recovery())
+	handler.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * 3600,
+	}))
 
 	// health check
 	handler.GET("/health", func(c *gin.Context) {
